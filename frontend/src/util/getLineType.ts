60175684ba5d94f8b7cbd273bbb47c98c89e24df
michalpazur <michal.pazur1@gmail.com>
2024-05-04 23:05:08 +0200

    Frontend: added support for subway

--- frontend/src/util/getLineType.ts
+++ frontend/src/util/getLineType.ts
@@ -1,6 +1,7 @@
 import { LineType } from "./lineColors";
 
 export const getLineType = (line: string): LineType => {
+  if (line === "M1" || line === "M2") return line;
   if (line.startsWith("N")) return "night";
   if (line.startsWith("S")) return "train";
 
--- frontend/src/util/lineColors.ts
+++ frontend/src/util/lineColors.ts
@@ -1,6 +1,6 @@
-import { grey, lightGreen, purple, red } from "@mui/material/colors";
+import { blue, grey, lightGreen, purple, red } from "@mui/material/colors";
 
-export type LineType = "bus" | "tram" | "night" | "train";
+export type LineType = "bus" | "tram" | "night" | "train" | "M1" | "M2";
 type LineTypeMap = {
   [line in LineType]: string;
 };
@@ -10,4 +10,6 @@ export const lineColors: LineTypeMap = {
   night: grey[900],
   tram: red[700],
   train: lightGreen[700],
+  M1: blue.A700,
+  M2: red.A700,
 };
